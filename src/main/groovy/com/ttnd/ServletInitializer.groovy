cc7c2cb5783aac908d4f77e5dd60e1aa5759127b
mansiarora <mansi@intelligrape.com>
2016-02-11 13:29:08 +0530

    Created new spring boot application

---
+++ src/main/groovy/com/ttnd/ServletInitializer.groovy
@@ -0,0 +1,13 @@
+package com.ttnd
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
+
+class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		application.sources(DemoApplication)
+	}
+
+}
